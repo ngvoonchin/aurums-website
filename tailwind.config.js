adb2e03ff26c362613f22972b247cbcbd6f30436
ngvoonchin <68941430+ngvoonchin@users.noreply.github.com>
2023-07-03 14:26:00 +0800

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "layered-waves-1": "url('/images/bg/layered-waves-1.svg')",
+        "blob-scene-1": "url('/images/bg/blob-scene-1.svg')"
+      },
+    },
+  },
+  plugins: [],
+}
